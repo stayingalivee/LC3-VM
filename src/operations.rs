f5fc160800f3761eb9cb18a074654a3c9d299b2a
Ahmad <n.ahmadalkhatib@gmail.com>
2020-11-20 00:08:11 +0300

    fix: handle overflows and negative values

--- src/operations.rs
+++ src/operations.rs
@@ -1,6 +1,8 @@
 use crate::register::Reg;
 use crate::register::Register;
 use crate::cond_flags::Cond_flags;
+use std::num::Wrapping;
+
 
 pub fn op_st(){
 
@@ -20,7 +22,12 @@ pub fn op_ld(){
  * instruction example
  * TODO: test this 
  * 0001 dr SR1 0 00 SR2 --> get the second value from SR2 register.
- * 0001 dr SR1 1 imm5   --> immediate mode, do DR1 + imm5 after sign extending. 
+ * 0001 dr SR1 1 imm5   --> immediate mode, do DR1 + imm5 after sign extending.
+ *               10001
+ * 1111111111111101 -> -3
+ * 0000000000000111 ->
+ * 
+ * a = b + c
  */
 pub fn op_add(reg: &mut Register, instr: u16){
     let dr = (instr >> 9) & 0b111;                      // destination register
@@ -29,10 +36,10 @@ pub fn op_add(reg: &mut Register, instr: u16){
 
     if mode == 1 {                                      // if immediate mode then fetch the data from the instruction itself.
         let imm5: u16 = sign_ext(instr & 0b11111, 5);
-        reg[dr] = reg[sr1] + imm5;
+        reg[dr] = reg[sr1].wrapping_add(imm5);
     } else {                                            // otherwise get the data from the register 
         let sr2 = instr & 0b111;
-        reg[dr] = reg[sr1] + reg[sr2];
+        reg[dr] = reg[sr1].wrapping_add(reg[sr2]);
     }
     update_flags(reg, dr);
 }
@@ -128,39 +135,55 @@ fn test_sign_ext() {
 #[test]
 fn test_op_add_imm(){
     let mut register  = Register {reg: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]};
-    let instr_wo_op: u16 = 0b001010100011;
-    op_add(&mut register, instr_wo_op);
+    let instr: u16 = 0b0001_001_010_1_00011;
+    op_add(&mut register, instr);
     assert_eq!(register[1], 3);
 }
 
 #[test]
 fn test_op_add(){
     let mut register  = Register {reg: [0, 0, 2, 3, 0, 0, 0, 0, 0, 0]};
-    let instr_wo_op: u16 = 0b001010000011;
-    op_add(&mut register, instr_wo_op);
+    let instr: u16 = 0b0001_001_010_0_00011;
+    op_add(&mut register, instr);
     assert_eq!(register[1], 5);
 }
 
 #[test]
 fn test_op_add_neg(){
     let mut register  = Register {reg: [0, 0, 0, 3, 0, 0, 0, 0, 0, 0]};
-    let instr_wo_op: u16 = 0b001010000011;
-    op_add(&mut register, instr_wo_op);
-    assert_eq!(register[1], 1);
+    let instr: u16 = 0b0001_001_010_0_00011;
+    op_add(&mut register, instr);
+    assert_eq!(register[1], 3);
 }
 
 #[test]
-fn test_op_add_sign_ext(){
+fn test_op_add_sign_ext_imm5(){
     let mut register  = Register {reg: [0, 0, 2, 0, 0, 0, 0, 0, 0, 0]};
-    let instr_wo_op: u16 = 0b001010111111;
-    op_add(&mut register, instr_wo_op);
-    assert_eq!(register[1], 1);
+    let instr: u16 = 0b_0001_001_010_1_11111;
+    op_add(&mut register, instr);
+    assert_eq!(register[1], 0b0000000000000001);
+}
+
+#[test]
+fn test_pos_flag(){
+    let mut register  = Register {reg: [0, 0, 4, 3, 0, 0, 0, 0, 0, 0]};
+    let instr: u16 = 0b0001_001_010_0_00011;
+    op_add(&mut register, instr);
+    assert_eq!(register[Reg::R_COND], 0b001);
 }
 
 #[test]
 fn test_neg_flag(){
-    let mut register  = Register {reg: [0, 0, 4, 3, 0, 0, 0, 0, 0, 0]};
-    let instr_wo_op: u16 = 0b001010000011;
-    op_add(&mut register, instr_wo_op);
-    assert_eq!(register[Reg::R_COND], 0b10);
+    let mut register  = Register {reg: [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]};
+    let instr: u16 = 0b0001_001_010_1_10011;
+    op_add(&mut register, instr);
+    assert_eq!(register[Reg::R_COND], 0b100);
+}
+
+#[test]
+fn test_zero_flag(){
+    let mut register  = Register {reg: [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]};
+    let instr: u16 = 0b0001_001_010_1_11111;
+    op_add(&mut register, instr);
+    assert_eq!(register[Reg::R_COND], 0b010);
 }
